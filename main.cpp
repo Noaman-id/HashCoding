46ff74dbeec2ec98261e5c67a168cc4516d213b5
Noaman-id <noemanidelmou4@gmail.com>
2025-04-03 09:09:55 +0000

    Version Final

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-#include <string>
-#include <cstdlib>
 #include <fstream>
+#include <string>
 #include <cmath>
+#include <cstdlib>
+
 using namespace std;
 
 #define m 100
@@ -15,7 +16,7 @@ struct Pliste
 struct Eliste
 {
 	int NbElem;
-	Pliste* Suiv; 
+	Pliste* Lien; 
 };
 struct TTHash
 {
@@ -24,8 +25,8 @@ struct TTHash
 
 int longfc(string Cle) {
     int count = 0;
-    while (Cle[count] != NULL) {  
-        count++;
+	while (Cle[count] != '\0' ) {
+		count++;
     }
     return count;
 }
@@ -37,23 +38,33 @@ int Fonc_Hashcode(string Cle, int nb_function){
 	{
 	case 1:
 		for(int i=0 ; i<L ;i++){
-			cp += (Cle[i] - '0') * 19 ? (Cle[i]>='0' && Cle[i]<='9') : (Cle[i] - '0') * 19;
+			if(Cle[i]>='0' && Cle[i]<='9'){
+				cp += (Cle[i] - '0') * 19;
+			}
+			else
+			{
+				cp += (Cle[i] - 'A') * 19;
+			}
 		}
 		return cp % m;
 		break;
 	case 2:
 		for(int i=0 ; i<L ;i++){
-			cp += (Cle[i] >= '0' && Cle[i] <= '9') ? pow((Cle[i] - '0'), 2) * 19 : pow((Cle[i] - '0') * 19, 2);
+			cp += (Cle[i] >= '0' && Cle[i] <= '9') ? pow((Cle[i] - '0'), 2) * 19 : pow((Cle[i] - 'A') * 19, 2);
 		}
 		return cp % m;
 		break;
 	case 3:
-
+		for(int i=0 ; i<L ;i++){
+			cp += (Cle[i] >= '0' && Cle[i] <= '9') ? pow((Cle[i] - '0'), 3) * 19 : pow((Cle[i] - 'A') * 19, 3);
+		}
+		return cp % m;	
 		break;
 	}
+	return cp % m;
 }
 
-void Ajouter_liste(Pliste* Ptr,string Cle){
+void Ajouter_liste(Pliste*& Ptr,string Cle){
 	Pliste* P;
 	Pliste* Q;
 	Pliste* R;
@@ -68,7 +79,7 @@ void Ajouter_liste(Pliste* Ptr,string Cle){
 			Ptr=Q;
 		}
 		else{
-			while (Cle > P->info && P != NULL)
+			while ( P != NULL && Cle > P->info )
 			{
 				R=P;
 				P=P->Suiv;
@@ -101,7 +112,7 @@ string CreeCle(int a,int b){
 
 int CreeFichier(int a){
 	ofstream fichier("Bib.bin", ios::out | ios::trunc);
-	for(int i=0;i<a;i++){
+	for(int i=0;i<=a;i++){
 		fichier<<CreeCle(3,30)<<endl;
 	}
 	return 1;
@@ -111,32 +122,68 @@ void Ajouter_Thash(TTHash& THash, string Cle, int NoFonction) {
 
     int Indice = Fonc_Hashcode(Cle, NoFonction);
 
-	Ajouter_liste(THash.Tableau[Indice].Suiv, Cle);
+	Ajouter_liste(THash.Tableau[Indice].Lien, Cle);
 
 	THash.Tableau[Indice].NbElem++;
+	
 }
 
+void Liberer(TTHash Thash) {
+	Pliste* P;
+	Pliste* Q;
+	for (int i = 0; i < m; i++) {
+		P = Thash.Tableau[i].Lien;
+		while (P != NULL) {
+			Q = P->Suiv;
+			delete P;
+			P = Q;
+		}
+	}
+}
 
+void Init_TabHashCode(TTHash& Thash) {
+	for (int i = 0; i < m; i++) {
+		Thash.Tableau[i].NbElem = 0;
+		Thash.Tableau[i].Lien = NULL;
+	}
+}
 
-void Creer_Thash(TTHash Thash, string Name, int NoFonction) 
+void Afficher_NbElem(TTHash Thash){
+	for(int i = 1 ; i < 5 ; i++){
+		for( int j = (i - 1) * 20; j < (i*20)-1; j++){
+			cout<<Thash.Tableau[j].NbElem <<" ";
+		}
+		cout<<endl;
+	}
+	cout<< endl;
+}
+
+void Creer_Thash(TTHash &Thash, string Name, int NoFonction) 
 {
-    ifstream F(Name);  
-    if (!F) {
+    ifstream File(Name);  
+    if (!File) {
         cerr << "Erreur : impossible d'ouvrir le fichier " << Name << std::endl;
         return;
     }
 
-
     string Cle;
-    while (F >> Cle) 
+    while (File >> Cle) 
 	{  
         Ajouter_Thash(Thash, Cle, NoFonction);
     }
 
-    F.close();  
+    File.close();  
 }
 
 int main() {
+	TTHash Thash;
+	string Name = "Bib.bin";
 	CreeFichier(10000);
+	for(int i = 1 ; i < 4 ; i++){
+		Init_TabHashCode(Thash);
+		Creer_Thash(Thash,Name,i);
+		Afficher_NbElem(Thash);
+		Liberer(Thash);
+	}
 	return 0;
 }
